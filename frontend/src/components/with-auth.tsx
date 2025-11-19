f59b2e7589efcf8c504c6f7bc5833d62d660095c
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-11-20 06:29:41 +0800

    feat(frontend): add sign out button

--- frontend/src/components/with-auth.tsx
+++ frontend/src/components/with-auth.tsx
@@ -1,9 +1,23 @@
 import { ErrorComponent } from "./error-component";
 import { Loading } from "./loading";
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from "./ui/alert-dialog";
+import { Button } from "./ui/button";
 import { useAuth } from "@/contexts/auth-provider";
+import { TopBarItem } from "@/contexts/use-top-bar";
 import type { ApiError } from "@/services";
-import { me } from "@/services/auth";
+import { me, signOut } from "@/services/auth";
 import { useQuery } from "@tanstack/react-query";
+import { TbLogout } from "react-icons/tb";
 import { Navigate, useLocation, useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 
@@ -42,7 +56,40 @@ function withAuth<P extends object>(Wrapped: React.ComponentType<P>) {
 			}
 		}
 
-		return <Wrapped {...props} />;
+		return (
+			<>
+				<TopBarItem side="right" id="sign-out-button">
+					<AlertDialog>
+						<AlertDialogTrigger asChild>
+							<Button variant="outline" size="icon">
+								<TbLogout />
+							</Button>
+						</AlertDialogTrigger>
+						<AlertDialogContent>
+							<AlertDialogHeader>
+								<AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+								<AlertDialogDescription>
+									You will be logged out of your account and will need to sign in again to access your data.
+								</AlertDialogDescription>
+							</AlertDialogHeader>
+							<AlertDialogFooter>
+								<AlertDialogCancel>Cancel</AlertDialogCancel>
+								<AlertDialogAction
+									onClick={() => {
+										navigate("/sign-in", { replace: true });
+										setTimeout(() => signOut(), 50);
+										toast.success("Signed out successfully");
+									}}
+								>
+									Continue
+								</AlertDialogAction>
+							</AlertDialogFooter>
+						</AlertDialogContent>
+					</AlertDialog>
+				</TopBarItem>
+				<Wrapped {...props} />;
+			</>
+		);
 	};
 }
 
