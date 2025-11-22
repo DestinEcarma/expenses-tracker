d7a283cbeac4518e82e4d1d090f14e7a559ea556
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-11-22 13:12:54 +0800

    refactor(backend): change path of create and list for both categories and transactions

--- backend/src/api/expenses/mod.rs
+++ backend/src/api/expenses/mod.rs
@@ -12,19 +12,20 @@ use axum::{
 use crate::api::{ApiState, auth::middleware::require_auth};
 
 pub fn router(state: Arc<ApiState>) -> Router<Arc<ApiState>> {
-    let categories_router = Router::new().route("/", post(categories::create));
+    let categories_router = Router::new().route("/create", post(categories::create));
     let category_router = Router::new()
         .route("/edit", patch(categories::edit))
         .route("/delete", delete(categories::delete));
 
-    let transactions_router =
-        Router::new().route("/", get(transactions::list).post(transactions::create));
+    let transactions_router = Router::new()
+        .route("/create", post(transactions::create))
+        .route("/list", get(transactions::list));
     let transaction_router = Router::new()
         .route("/edit", patch(transactions::edit))
         .route("/delete", delete(transactions::delete));
 
     Router::new()
-        .route("/", get(handlers::get_expenses_overview))
+        .route("/overview", get(handlers::get_expenses_overview))
         .nest(
             "/categories",
             categories_router.nest(
